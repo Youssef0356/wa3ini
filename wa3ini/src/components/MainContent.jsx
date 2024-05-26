2bb2f0d0f42652deae22cf70742a98f1dbaeddf3
Youssef0356 <nejiyoussef081@gmail.com>
2024-05-26 21:52:33 +0100

    i changed the wallpaper  and added title with some modifications

--- wa3ini/src/components/MainContent.jsx
+++ wa3ini/src/components/MainContent.jsx
@@ -1,4 +1,4 @@
-
+import Header from "./Header.jsx";
 function Card() {
     return (
       <div className="bg-pink-500 lg:col-span-3 md:col-span-4 sm:col-span-6 col-span-12"></div>
@@ -8,7 +8,7 @@ function Card() {
 export default function MainContent() {
     return <>
       <div className=" bg-blue-500 sm:ml-64">
-        <div className="h-64 bg-yellow-500"></div>
+        <div className="h-64"> <Header /></div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
           <div className="bg-pink-500 h-48"><button>autism</button></div>
           <div className="bg-pink-500 h-48"><button>no hear</button></div>
