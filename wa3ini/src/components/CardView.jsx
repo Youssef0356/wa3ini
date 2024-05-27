57a7284660d2672f88cdc8576852989d6c33828a
Youssef0356 <nejiyoussef081@gmail.com>
2024-05-27 11:15:47 +0100

    imported first image

--- wa3ini/src/components/CardView.jsx
+++ wa3ini/src/components/CardView.jsx
@@ -1,3 +1,4 @@
+import autism from "../assets/autism.jpg";
 export default function CardView() {
   return (
     <>
@@ -15,11 +16,10 @@ export default function CardView() {
           <div class="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
             <img
               class="h-56 lg:h-60 w-full object-cover"
-              src="https://images.unsplash.com/photo-1523289217630-0dd16184af8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBlbXBvd2VybWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+              src={autism}
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
@@ -41,7 +41,6 @@ export default function CardView() {
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
@@ -62,7 +61,6 @@ export default function CardView() {
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
@@ -84,7 +82,6 @@ export default function CardView() {
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
@@ -106,7 +103,6 @@ export default function CardView() {
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
@@ -128,7 +124,6 @@ export default function CardView() {
               alt=""
             />
             <div class="p-3">
-              <span class="text-sm text-primary">November 19, 2022</span>
               <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
                 International Women's Day 2022: Date, history, significance,
                 theme this year
