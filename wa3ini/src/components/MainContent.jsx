63e3a4de767faff414d84a50fc5f63eb4324b61e
Youssef0356 <nejiyoussef081@gmail.com>
2024-05-27 10:58:54 +0100

    grid view is kinda done

--- wa3ini/src/components/MainContent.jsx
+++ wa3ini/src/components/MainContent.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header.jsx";
+import CardView from "./CardView.jsx";
 function Card() {
     return (
       <div className="bg-pink-500 lg:col-span-3 md:col-span-4 sm:col-span-6 col-span-12"></div>
@@ -9,17 +10,7 @@ export default function MainContent() {
     return <>
       <div className=" bg-blue-500 sm:ml-64">
         <div className="h-64"> <Header /></div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
-          <div className="bg-pink-500 h-48"><button>autism</button></div>
-          <div className="bg-pink-500 h-48"><button>no hear</button></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-          <div className="bg-pink-500 h-48"></div>
-        </div>
+        <CardView />
       </div>
     </>
   }
